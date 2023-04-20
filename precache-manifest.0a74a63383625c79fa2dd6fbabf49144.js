b5cb06f546c1f294ef402a132bbed69f6035ae23
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-04-20 16:10:30 +0100

    Updates

---
+++ precache-manifest.0a74a63383625c79fa2dd6fbabf49144.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "433bd465dc8f23f15f31",
+    "url": "/client.github.io/static/css/main.90eb9d5d.chunk.css"
+  },
+  {
+    "revision": "433bd465dc8f23f15f31",
+    "url": "/client.github.io/static/js/main.433bd465.chunk.js"
+  },
+  {
+    "revision": "a3e83d269b4ee0699568",
+    "url": "/client.github.io/static/js/1.a3e83d26.chunk.js"
+  },
+  {
+    "revision": "7fff0aa95345898324cc",
+    "url": "/client.github.io/static/js/2.7fff0aa9.chunk.js"
+  },
+  {
+    "revision": "f1c00031b0a85a4561a9",
+    "url": "/client.github.io/static/js/runtime~main.f1c00031.js"
+  },
+  {
+    "revision": "6562cb62d0241e03e68924b953972428",
+    "url": "/client.github.io/static/media/1.6562cb62.svg"
+  },
+  {
+    "revision": "4ef1f7902519369328d2f767e013a660",
+    "url": "/client.github.io/static/media/2.4ef1f790.svg"
+  },
+  {
+    "revision": "b15e7e37d9bd806835e12ae535df3d4f",
+    "url": "/client.github.io/static/media/icon_search.b15e7e37.svg"
+  },
+  {
+    "revision": "9aea806dbf838f574307a1cdaa30d468",
+    "url": "/client.github.io/static/media/bell.9aea806d.svg"
+  },
+  {
+    "revision": "d74b10fc0e0db96ad6446a2e65dc35b7",
+    "url": "/client.github.io/static/media/msg.d74b10fc.svg"
+  },
+  {
+    "revision": "2a190c7b14582aaa70edb36c067b43d8",
+    "url": "/client.github.io/index.html"
+  }
+];
