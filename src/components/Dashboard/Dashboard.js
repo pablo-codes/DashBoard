a36a38a1c104bb3f0cddc7dcc82e4798e35d548b
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-05-16 13:19:39 +0100

    debugging

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import DashNav from './body/DashNav'
 import './css/bootstrap1.css'
-import './css/Dashboard.css'
 import './css/style1.css'
 import './css/metisMenu.css'
 import DashBody from './body/DashBody'
